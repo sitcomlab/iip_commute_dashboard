c669302ee15607dc518c59baa95da07b8d73e833
Felix Erdmann <hello@felixerdmann.com>
2022-11-17 13:55:23 +0100

    inital commit

---
+++ components/Hello.tsx
@@ -0,0 +1,9 @@
+export default function Hello() {
+  const a = 0
+  const b = ''
+  return (
+    <h1 className="bg-msGreen text-3xl font-bold text-white underline ">
+      Hello world!
+    </h1>
+  )
+}
