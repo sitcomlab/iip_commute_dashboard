f33d8f9f06043d843b77582b85496b49508062c2
Felix Erdmann <hello@felixerdmann.com>
2022-12-08 16:45:08 +0100

    feat: extract stadtradeln chart to own component

--- components/Tiles/Mobility/Bicycle/Stadtradeln/index.tsx
+++ components/Tiles/Mobility/Bicycle/Stadtradeln/index.tsx
@@ -1,41 +1,11 @@
-'use client'
-
-import Switch from '@/components/Inputs/Switch'
 import MobilityTile from '@/components/Tiles/Base/MobilityTile'
-import { useState } from 'react'
-import Chart from './Chart'
+import ChartContainer from './ChartContainer'
 
 export default function StadtradelnTile() {
-  const [compare, setCompare] = useState(false)
-
   return (
     <MobilityTile subtitle="auf der Überholspur" title="Stadtradeln">
-      <div className="rounded bg-white p-2">
-        <div className="flex h-96 w-full items-center justify-center">
-          <Chart compare={compare} />
-        </div>
-        <div className="flex items-center space-x-4 p-4">
-          <div className="h-1 w-12 rounded bg-mobility" />
-          <p className="text-primary">geradelte Kilometer in Münster</p>
-        </div>
-        <div className="flex w-full flex-col justify-between space-y-2 rounded border border-dashed border-primary p-4 lg:flex-row lg:space-x-2">
-          <Switch
-            defaultChecked={compare}
-            label="Städtevergleich"
-            onCheckedChange={setCompare}
-            variant={'mobility'}
-          />
-          <div className="flex items-center space-x-4">
-            <div className="h-1 w-12 rounded bg-energy" />
-            <p className="text-primary">Freiburg</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="h-1 w-12 rounded bg-buildings" />
-            <p className="text-primary">Konstanz</p>
-          </div>
-        </div>
-      </div>
-      <p className="my-2 font-semibold">
+      <ChartContainer />
+      <p className="prose my-2 font-semibold">
         Auf die Leeze, fertig, los! Für mehr Radverkehr, Klimaschutz und
         Lebensqualität in die Pedale treten: Seit 2020 beteiligt sich die Stadt
         Münster an der jährlich stattfindenden, dreiwöchigen bundesweiten Aktion
