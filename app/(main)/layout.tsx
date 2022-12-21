58294fef7b8fb401693243914ec0313131a294b5
Felix Erdmann <hello@felixerdmann.com>
2022-12-21 22:21:24 +0100

    feat: page height screen

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,10 +4,12 @@ import Providers from '@/components/Layout/Providers'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex h-screen flex-col">
       <Navbar />
-      <Providers>{children}</Providers>
+      <div className="flex-1">
+        <Providers>{children}</Providers>
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
