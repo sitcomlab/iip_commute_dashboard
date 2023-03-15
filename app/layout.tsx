c5799b5b541b900a64a1965b541a3372b4a1729e
Felix Erdmann <hello@felixerdmann.com>
2023-03-15 14:44:40 +0100

    chore: codemod next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
