9d088cd61a3a6c84370acd53b160d9eb04455c31
Felix Erdmann <hello@felixerdmann.com>
2023-03-15 14:48:48 +0100

    feat: use next metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
+export const metadata = {
+  title: 'Klimadashboard Münster',
+  viewport: 'width=device-width, initial-scale=1',
+  description: 'Das Klimadashboard der Stadt Münster',
+  icons: '/favicon.ico',
+}
+
 export default function RootLayout({
   children,
 }: {
