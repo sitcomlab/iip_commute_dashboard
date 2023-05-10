0ab34c70199ba5788eaa26a72a8778d6f98efd9a
Felix Erdmann <hello@felixerdmann.com>
2023-05-10 12:16:33 +0200

    feat: add footer (without logos for now)

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,18 +1,110 @@
 import Link from 'next/link'
 import StairStepBackground from '../StairStepBackground'
+import Script from 'next/script'
+import Title from '@/components/Elements/Title'
+import Container from '../Container'
 
 export default function Footer() {
   return (
     <StairStepBackground>
-      <div className="flex w-full flex-col items-center justify-around  sm:flex-row">
-        <div>Ein Projekt von</div>
-        <div>Gefördert durch</div>
-        <div className="flex flex-col">
-          <Link href="#">Impressum</Link>
-          <Link href="#">Datenschutz</Link>
-          <Link href="#">Sie haben Feedback für uns?</Link>
+      <Script id="linkTo_UnCryptMailto">
+        {`function UnCryptMailto( s )
+          {
+            var n = 0;
+            var r = "";
+            for( var i = 0; i < s.length; i++)
+            {
+                n = s.charCodeAt( i );
+                if( n >= 8364 )
+                {
+                    n = 128;
+                }
+                r += String.fromCharCode( n - 1 );
+            }
+            return r;
+        }
+
+        function linkTo_UnCryptMailto( s )
+        {
+            location.href=UnCryptMailto( s );
+        }
+      `}
+      </Script>
+      <Container>
+        <div className="items-top flex w-full flex-col justify-around pb-10 lg:flex-row lg:gap-20 xl:pb-20 2xl:gap-44">
+          <div className="flex-1">
+            <Title as={'h4'} className="py-10 md:py-20">
+              Ein Projekt von
+            </Title>
+            <div className="flex w-full grid-cols-2 flex-col gap-8 lg:gap-16 xl:grid">
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+            </div>
+          </div>
+          <div className="flex-1">
+            <Title as={'h4'} className="py-10 md:py-20">
+              Gefördert durch
+            </Title>
+            <div className="flex w-full grid-cols-2 flex-col gap-8 lg:gap-16 xl:grid">
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+              <div className="h-28 w-full bg-red-300 md:h-36">
+                {/* TODO: ADD IMAGE */}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-1 flex-col sm:hidden 2xl:flex">
+            <Title as={'h4'} className="py-10 opacity-0 md:py-20">
+              Impressum
+            </Title>
+            <div className="flex flex-1 flex-col justify-between gap-8">
+              <Link href="https://www.stadt-muenster.de/impressum">
+                <Title as="h5" className="underline" variant={'primary'}>
+                  Impressum
+                </Title>
+              </Link>
+              <Link href="https://www.stadt-muenster.de/datenschutz">
+                <Title as="h5" className="underline" variant={'primary'}>
+                  Datenschutz
+                </Title>
+              </Link>
+              <a href="javascript:linkTo_UnCryptMailto('nbjmup;gffecbdlAlmjnbebticpbse/nt');">
+                <Title as="h5" className="underline" variant={'primary'}>
+                  Sie haben Feedback für uns?
+                </Title>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="hidden justify-between sm:flex 2xl:hidden">
+          <Link href="https://www.stadt-muenster.de/impressum">
+            <Title as="h5" className="underline" variant={'primary'}>
+              Impressum
+            </Title>
+          </Link>
+          <Link href="https://www.stadt-muenster.de/datenschutz">
+            <Title as="h5" className="underline" variant={'primary'}>
+              Datenschutz
+            </Title>
+          </Link>
+          <a href="javascript:linkTo_UnCryptMailto('nbjmup;gffecbdlAlmjnbebticpbse/nt');">
+            <Title as="h5" className="underline" variant={'primary'}>
+              Sie haben Feedback für uns?
+            </Title>
+          </a>
+        </div>
+      </Container>
     </StairStepBackground>
   )
 }
--- components/Tiles/Mobility/Bicycle/BicycleChartTile/BicycleProgress.tsx
+++ components/Tiles/Mobility/Bicycle/BicycleChartTile/BicycleProgress.tsx
@@ -2,13 +2,18 @@ import { BicycleEnd, BicycleStart } from '@/components/Icons'
 import * as ProgressPrimitive from '@radix-ui/react-progress'
 
 export default function BicycleProgress({ progress }: { progress: number }) {
+  const myProgress = progress > 0 ? progress : 0
+
   return (
     <div className="flex w-full">
       <BicycleStart className="h-6 -translate-y-[3px] translate-x-0.5 md:h-12 md:translate-x-1" />
-      <ProgressPrimitive.Root className="w-[calc(100%-4rem)]" value={progress}>
+      <ProgressPrimitive.Root
+        className="w-[calc(100%-4rem)]"
+        value={myProgress}
+      >
         <ProgressPrimitive.Indicator
           className="relative mt-0.5 flex h-1.5 bg-mobility transition-all md:mt-2 md:h-3"
-          style={{ width: `${progress}%` }}
+          style={{ width: `${myProgress}%` }}
         >
           <BicycleEnd className="absolute -right-4 top-0 h-6 -translate-y-[5.2px] md:-right-8 md:h-12 md:-translate-y-[11.3px]" />
         </ProgressPrimitive.Indicator>
