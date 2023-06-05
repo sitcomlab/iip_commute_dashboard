ac0153b3fc518cb1195f0901be048283a8ae4b51
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-06-05 12:39:52 +0200

    chore: update bus icons

---
+++ components/Icons/EBusBrennstoff.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+function SvgEBusBrennstoff(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 520.863 203.488"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        data-name="Gruppe 875"
+        fill="none"
+        stroke="#005b79"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2.5}
+      >
+        <g data-name="Gruppe 64" transform="translate(1.25 1.25)">
+          <path
+            d="M297.646 175.086h29.181a11.707 11.707 0 0 0 11.673-11.672V11.672A11.707 11.707 0 0 0 326.827 0H11.673A11.707 11.707 0 0 0 0 11.672v151.742a11.707 11.707 0 0 0 11.673 11.672h29.181"
+            data-name="Pfad 22"
+          />
+          <path d="M97.314 174.19h142.925" data-name="Linie 14" />
+          <ellipse
+            cx={29.032}
+            cy={26.798}
+            data-name="Ellipse 9"
+            rx={29.032}
+            ry={26.798}
+            transform="translate(240.239 147.391)"
+          />
+          <circle
+            cx={26.798}
+            cy={26.798}
+            data-name="Ellipse 10"
+            r={26.798}
+            transform="translate(43.717 147.391)"
+          />
+          <path
+            d="M0 34.959h309.318a5.838 5.838 0 0 1 5.836 5.836v40.854a5.838 5.838 0 0 1-5.836 5.836H0"
+            data-name="Pfad 23"
+          />
+          <path d="M200.041 38.413v49.13" data-name="Linie 15" />
+          <path d="M258.104 38.413v49.13" data-name="Linie 16" />
+          <path d="M141.978 38.413v49.13" data-name="Linie 17" />
+          <path d="M79.448 38.413v49.13" data-name="Linie 18" />
+        </g>
+        <g data-name="Gruppe 67">
+          <path
+            d="M473.622 129.73h10.821a35.17 35.17 0 1 0 0-70.339c-.541 0-1.055.054-1.569.081a37.866 37.866 0 0 0-74.18 10.74 36.693 36.693 0 0 0 .6 6.493 27.006 27.006 0 0 0-14.095 42.2"
+            data-name="Pfad 65"
+          />
+          <path
+            d="M362.704 129.734h83.865a24.348 24.348 0 1 0-17.217-41.565"
+            data-name="Pfad 66"
+          />
+          <path
+            d="M362.704 140.551h56.812a18.937 18.937 0 1 1-13.391 32.329"
+            data-name="Pfad 67"
+          />
+        </g>
+      </g>
+    </svg>
+  )
+}
+export default SvgEBusBrennstoff
