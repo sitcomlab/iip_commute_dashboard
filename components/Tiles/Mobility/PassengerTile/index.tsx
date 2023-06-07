e6825b9f42f6265b3ae1755ae61f81990525d158
Matthias Pfeil <matthias.pfeil@wwu.de>
2023-06-07 12:10:14 +0200

    chore: fix opnv tile id

--- components/Tiles/Mobility/PassengerTile/index.tsx
+++ components/Tiles/Mobility/PassengerTile/index.tsx
@@ -16,7 +16,7 @@ export default function PassengerTile() {
     <MobilityTile
       dataRetrieval={format(new Date('2023-05-22T00:00:00.000Z'), 'dd.MM.yyyy')}
       dataSource={'Stadtwerke Münster'}
-      embedId="mobility-masterplan"
+      embedId="mobility-passengers"
       subtitle={'Entwicklung der Fahrgastzahlen in den Bussen der Stadtwerke'}
       title={'ÖPNV'}
     >
