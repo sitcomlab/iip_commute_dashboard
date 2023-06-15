10e9c265e7a3c81aa74501308f297365e31ddf01
Felix Erdmann <hello@felixerdmann.com>
2023-06-15 14:13:37 +0200

    fix: data count tile half width

--- components/Views/ClimateView.tsx
+++ components/Views/ClimateView.tsx
@@ -20,8 +20,9 @@ export default function ClimateView() {
       <ClimateIndicesTile />
 
       <GarbageTile />
-
-      <DataCountTile />
+      <Columns>
+        <DataCountTile />
+      </Columns>
     </BaseView>
   )
 }
