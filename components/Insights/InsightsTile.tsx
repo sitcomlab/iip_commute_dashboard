e50e2a6968414dda8f842d461117f19503c81309
Felix Erdmann <hello@felixerdmann.com>
2023-06-15 12:59:59 +0200

    fix: hover background for insights tile

--- components/Insights/InsightsTile.tsx
+++ components/Insights/InsightsTile.tsx
@@ -16,16 +16,20 @@ export default function InsightsTile({
   return (
     <div className="h-full overflow-hidden rounded-[36px] bg-white md:rounded-[56px]">
       <Link href={link}>
-        <div className="group flex h-full w-full flex-col  bg-secondary transition-all hover:bg-opacity-30">
-          <div className="flex w-full flex-1 justify-between gap-2 py-[33px] pl-[52px] pr-12 group-hover:text-secondary">
+        <div className="group flex h-full w-full flex-col  bg-secondary transition-all hover:bg-[#f6f7e6]">
+          <div className="flex w-full flex-1 justify-between gap-2 py-[33px] pl-[52px] pr-12">
             <Title
               as="h6"
-              className="mt-2 flex-[2_2_0%] underline"
+              className="mt-2 flex-[2_2_0%] underline group-hover:text-secondary"
               variant={'inverse'}
             >
               Kacheln filtern
             </Title>
-            <Title as={'h4'} className="flex-[3_3_0%]" variant="inverse">
+            <Title
+              as={'h4'}
+              className="flex-[3_3_0%] group-hover:text-secondary"
+              variant="inverse"
+            >
               {title}
             </Title>
           </div>
