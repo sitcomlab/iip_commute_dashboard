63797881aef9df7cfd61578fbf65e494e39e8a59
Felix Erdmann <hello@felixerdmann.com>
2023-06-16 10:33:38 +0200

    feat: add matomo

---
+++ app/Cookies.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import CookieConsent from 'react-cookie-consent'
+
+export default function Cookies() {
+  return (
+    <CookieConsent enableDeclineButton>Einwilligung zu Cookies</CookieConsent>
+  )
+}
