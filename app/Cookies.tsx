43719044cd22131f78c1a8530988a0dff1c797ae
Felix Erdmann <hello@felixerdmann.com>
2023-06-19 12:58:44 +0200

    feat: cookie notice style

--- app/Cookies.tsx
+++ app/Cookies.tsx
@@ -5,9 +5,20 @@ import CookieConsent from 'react-cookie-consent'
 export default function Cookies() {
   return (
     <CookieConsent
+      buttonStyle={{
+        background: '#aab315',
+        color: '#fff',
+        borderRadius: '4px',
+      }}
       buttonText="Akzeptieren"
+      declineButtonStyle={{
+        borderRadius: '4px',
+      }}
       declineButtonText="Ablehnen"
       enableDeclineButton
+      style={{
+        background: '#005b79',
+      }}
     >
       Einwilligung zu Cookies (Analytics, Tracking)
     </CookieConsent>
