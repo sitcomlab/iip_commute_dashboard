e1e48af22d4c46e5075f35d920585137a8b46b70
caro niebl <caro.niebl@uni-muenster.de>
2023-10-20 15:11:38 +0200

    fix bad css-keys

--- components/Icons/TubeIcon.tsx
+++ components/Icons/TubeIcon.tsx
@@ -3,34 +3,34 @@ import * as React from 'react'
 import  { SVGProps } from 'react'
 function SvgTubeIcon(props: SVGProps<SVGSVGElement>) {
     return <svg
-   viewBox="0 0 56.9 56.9"
-   id="Ebene_1"
    data-name="Ebene 1"
+   id="Ebene_1"
    version="1.1"
+   viewBox="0 0 56.9 56.9"
    xmlns="http://www.w3.org/2000/svg" {...props} >
   <defs></defs>
   <path
      d="m 5.3400662,28.230133 c 0,-12.686798 10.2847698,-22.9715967 22.9715958,-22.9715967 12.686826,0 22.971596,10.2847987 22.971596,22.9715967 0,12.686798 -10.28477,22.971597 -22.971596,22.971597 -12.686826,0 -22.9715958,-10.284799 -22.9715958,-22.971597 z"
-     fill-rule="evenodd"
-     id="path170"
      fill="none"
-     stroke-width="4.05681"
-     stroke="#e2f0d9"/>
+     fillRule="evenodd"
+     id="path170"
+     stroke="#e2f0d9"
+     strokeWidth="4.05681"/>
   <path
      d="m 37.15835,38.479692 6.532276,7.002335"
-     stroke-width="3.5708"
      id="path1601"
-     stroke="#e2f0d9"/>
+     stroke="#e2f0d9"
+     strokeWidth="3.5708"/>
   <path
      d="m 35.535174,40.658689 -3.802181,-6.199477 2.092341,-1.665141 5.894088,4.534336 z"
-     stroke="#e2f0d9"
-     stroke-width="2.80902"
-     stroke-linecap="round"
-     stroke-linejoin="round"
-     stroke-miterlimit="10"
      fill="#e2f0d9"
-     fill-rule="evenodd"
-     id="path2106"/>
+     fillRule="evenodd"
+     id="path2106"
+     stroke="#e2f0d9"
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeMiterlimit="10"
+     strokeWidth="2.80902"/>
 </svg>
 }
 export {SvgTubeIcon}
--- components/Icons/WayfindingIcon.tsx
+++ components/Icons/WayfindingIcon.tsx
@@ -3,31 +3,31 @@ import * as React from 'react'
 import  { SVGProps } from 'react'
 function SvgWayfindingIcon(props: SVGProps<SVGSVGElement>) {
     return <svg
-   viewBox="0 0 56.9 56.9"
-   id="Ebene_1"
    data-name="Ebene 1"
+   id="Ebene_1"
    version="1.1"
+   viewBox="0 0 56.9 56.9"
    xmlns="http://www.w3.org/2000/svg" {...props} >
   <defs></defs>
   <path
      d="M 29.554,20.123133 V 52.888077"
-     stroke-width="4.84208"
-     stroke-linecap="round"
-     fill-rule="evenodd"
-     id="path1522"/>
+     fillRule="evenodd"
+     id="path1522"
+     strokeLinecap="round"
+     strokeWidth="4.84208"/>
   <path
      d="m 29.04,18.87956 -12.99,-10.024 0.523,-6.352 6.366,0.325 6.533,7.874 z"
-     stroke-width="1.33333"
-     fill-rule="evenodd"
-     id="path1524"/>
+     fillRule="evenodd"
+     id="path1524"
+     strokeWidth="1.33333"/>
   <path
      d="m 29.554,20.27856 h -14.5 l -4.5,-4.5 4.5,-4.5 h 14.5 z"
-     stroke-width="1.33333"
-     id="path1526" />
+     id="path1526"
+     strokeWidth="1.33333" />
   <path
      d="m 29.554,11.27856 h 14.5 l 4.5,4.5 -4.5,4.5 h -14.5 z"
-     stroke-width="1.33333"
-     id="path1528" />
+     id="path1528"
+     strokeWidth="1.33333" />
 </svg>
 }
 export {SvgWayfindingIcon}
