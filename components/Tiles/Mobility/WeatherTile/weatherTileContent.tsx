54e518e00c863fe6b42c4f0c66938c92fe52d66f
niebl <caro.niebl@uni-muenster.de>
2023-12-07 10:55:57 +0100

    feat: weather conditions and icons

--- components/Tiles/Mobility/WeatherTile/weatherTileContent.tsx
+++ components/Tiles/Mobility/WeatherTile/weatherTileContent.tsx
@@ -4,6 +4,12 @@ import useWeatherData from '@/hooks/useWeatherData';
 import styled from 'styled-components';
 
 import { Temperature } from '@/components/Icons';
+import { Eis } from '@/components/Icons';
+import { Sun } from '@/components/Icons';
+import { Water } from '@mui/icons-material';
+import { Cloud } from '@mui/icons-material';
+import { EnergyIcon } from '@/components/Icons';
+
 
 const InlineIcon = styled.span`
     background-color: ${(props) => props.color};
@@ -16,18 +22,43 @@ function toCelsius(kelvin: number){
     return Math.round(celsius*10)/10
 }
 
-function getWeatherIcon(weather){
-    const wid = weather.weather.id
+function getWeatherCondition(weather){
+    const wid = weather.weather[0].id
     //see https://openweathermap.org/weather-conditions
-    let icon
+    let icon = Sun
+    let text = ''
     switch(true){
-        case (wid >= 300): //thunderstorm
-            return true;
+        case (wid >= 200 && wid <= 232): //thunderstorm
+            text = 'Gewitter'
+            icon = EnergyIcon
             break;
-
+        case (wid >= 300 && wid <= 321): //drizzle
+            text = 'leichter Regen'
+            icon = Water
+            break;
+        case (wid >= 500 && wid <= 531): //rain
+            text = 'Regen'
+            icon = Water
+            break;
+        case (wid >= 600 && wid <= 622): //snow
+            text = 'Schnee'
+            icon = Eis
+            break;
+        case (wid >= 700 && wid <= 781): //atmosphere (mist / fog)
+            text = 'Nebel'
+            icon = Cloud;
+            break;
+        case (wid == 800): //clear
+            text = 'klar'
+            icon = Sun
+            break;
+        case (wid >= 801 && wid <= 804): //clouds
+            text = 'bewölkt'
+            icon = Cloud;
+            break;
     }
 
-    return icon
+    return [text, icon]
 }
 
 function WeatherTileContent(props: {lat: string; lon: string}){
@@ -37,10 +68,11 @@ function WeatherTileContent(props: {lat: string; lon: string}){
     }
     const trueTemp = toCelsius(weather.main.temp)
     const perceivedTemp = toCelsius(weather.main.feels_like)
-
+    const [weatherText, WeatherIcon] = getWeatherCondition(weather)
+    console.log(weatherText)
 
     return(
-        <div className="flex justify">
+        <div className="flex" style={{justifyContent: 'space-around'}}>
         <div className="flex flex-col text-primary">
             <div style={{'display': 'flex', 'align-items': 'center'}}>
                 <InlineIcon><Temperature /></InlineIcon>
@@ -49,6 +81,8 @@ function WeatherTileContent(props: {lat: string; lon: string}){
             <div>feels like: <Title as="h5">{perceivedTemp}°C</Title></div>
         </div>
         <div className="flex flex-col items-end">
+            <WeatherIcon />
+            <Title as="h4">{weatherText}</Title>
         </div>
         {/* 
         {'test: lat:'+props.lat+' lon:'+props.lon}
