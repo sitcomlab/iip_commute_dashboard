c651af43c930e8e109e187fa7bbf42a2dd5b8012
niebl <caro.niebl@uni-muenster.de>
2024-04-09 12:08:53 +0200

    feat: use OSM basemap instead of Münsters

--- components/Tiles/Mobility/BikeInfrastructTile/BikeInfrastructTileContent.tsx
+++ components/Tiles/Mobility/BikeInfrastructTile/BikeInfrastructTileContent.tsx
@@ -93,8 +93,8 @@ function BikeInfrastructTileContent(props: { city: string; }) {
             zoom={city.mapSettings.zoom}
         >
             <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://geo.stadt-muenster.de/basiskarte/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
               
             <BicycleInfrastructureData map={map}/>
